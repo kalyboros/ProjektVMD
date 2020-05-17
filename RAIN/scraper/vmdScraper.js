adf22a45b498942d1a7e03e69e7f2654de35520c
DESKTOP-4HFK5JD\Vito <vito.kurnik@gmail.com>
2020-05-17 18:31:55 +0200

    implement basic scraper to get data from tables on website

---
+++ RAIN/scraper/vmdScraper.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+const url = 'https://www.promet.si/portal/sl/stevci-prometa.aspx';
+const $ = require('cheerio');
+
+puppeteer
+  .launch()
+  .then(function(browser) {
+    return browser.newPage();
+  })
+  .then(function(page) {
+    return page.goto(url).then(function() {
+      return page.content();
+    });
+  })
+  .then(function(html) {
+    $('td', html).each(function() {
+      console.log($(this).text());
+    });
+  })
+  .catch(function(err) {
+    //handle error
+  });
