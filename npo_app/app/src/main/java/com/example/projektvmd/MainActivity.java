ec8250f1d494c71c29e6c692f0bf91613c28d366
kalybor <dadosa123@gmail.com>
2020-05-18 19:14:13 +0200

    popravljeno pošiljanje POST zahtevka

--- npo_app/app/src/main/java/com/example/projektvmd/MainActivity.java
+++ npo_app/app/src/main/java/com/example/projektvmd/MainActivity.java
@@ -2,11 +2,19 @@ package com.example.projektvmd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -14,7 +22,7 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-    OkHttpClient client = new OkHttpClient();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,32 +30,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public static final MediaType JSON
-            = MediaType.parse("application/json; charset=utf-8");
+    public void sendToServer(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "Poslano v bazo";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
 
-    String doPostRequest(String url, String json) throws IOException {
-        RequestBody body = RequestBody.create(JSON, json);
+        try {
+            postRequest();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void postRequest() throws IOException {
+
+        MediaType MEDIA_TYPE = MediaType.parse("application/json");
+        String url = "https://df06d7d1.ngrok.io";
+
+        OkHttpClient client = new OkHttpClient();
+
+        JSONObject postdata = new JSONObject();
+        try {
+            postdata.put("username", "aneh");
+            postdata.put("password", "12345");
+        } catch(JSONException e){
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        RequestBody body = RequestBody.create(MEDIA_TYPE, postdata.toString());
+
         Request request = new Request.Builder()
                 .url(url)
                 .post(body)
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
                 .build();
 
-        Response response = client.newCall(request).execute();
-        return response.body().string();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                String mMessage = e.getMessage().toString();
+                Log.w("failure Response", mMessage);
+                //call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                String mMessage = response.body().string();
+                Log.e("on response", mMessage);
+            }
+        });
     }
-
-    public void sendToServer(View view) {
-        String latitude = "243";
-        //String postResponse = doPostRequest("http://www.roundsapp.com/post", latitude);
-        RequestBody body = RequestBody.create(JSON, latitude);
-        Request request = new Request.Builder()
-                .url("http://www.roundsappbbbccc.com/post")
-                .post(body)
-                .build();
-
-        //System.out.println(postResponse);
-
-    }
-
-
 }
