4842d1a90e3f1cbf3d0d255fc1d1e9cbf80f1bc7
DESKTOP-4HFK5JD\Vito <vito.kurnik@gmail.com>
2020-06-01 18:13:29 +0200

    popravki pri izrisu poti

--- RAIN/npo_gps/myscript.js
+++ RAIN/npo_gps/myscript.js
@@ -9,7 +9,7 @@ var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
   var pot = (__dirname + '/');
-  pot = pot.substring(0,pot.length - 3);
+  pot = pot.substring(0,pot.length);
   res.sendFile( pot + 'gps.html');
 });
 
@@ -31,11 +31,11 @@ MongoClient.connect("mongodb://127.0.0.1:27017", function (err, client) {
     if (err) throw err;
     var dolzina = result.length;
 
-    /*
+
     var nekaj = result[1];
     nekaj = nekaj._id.toString();
     console.log(nekaj);
-    */
+
     var i;
     for(i = 0; i < dolzina; i++){
       var longS = result[i];
@@ -53,12 +53,23 @@ MongoClient.connect("mongodb://127.0.0.1:27017", function (err, client) {
   });
 
 });
-
+/*
+longitudes.push("15.653236");
+longitudes.push("15.653150");
+longitudes.push("15.653816");
+longitudes.push("15.654954");
+longitudes.push("15.655018");
+latitudes.push("46.563006");
+latitudes.push("46.563788");
+latitudes.push("46.564349");
+latitudes.push("46.564349");
+latitudes.push("46.559943");
+*/
 io.on('connection', (socket) => {
   console.log('povezal se je odjemalec');
 
   socket.on('RequestUpdate', (msg) => {
-    //console.log(n);
+    console.log("zahtevek");
     //moram poracunat razlike v stanju vozisc, funkcija
     //posljem na socket
     io.emit('izris', longitudes, latitudes);
